9fbb6f4a2c52410f455dde41ceadcc11cd2c74e6
Kavia AI <production@kavia.ai>
2026-02-12 04:23:28 +0000

    Checkpoint 1 - cga1d1d494

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/smart-calendar-319576-319585/calendar_backend
+dotnet build --no-restore -v quiet -nologo -consoleloggerparameters:NoSummary /p:TreatWarningsAsErrors=false
+LINT_EXIT_CODE=$?
+if [ $LINT_EXIT_CODE -ne 0 ]; then
+  exit 1
+fi
+
